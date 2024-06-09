df469e4da11932d4407101a47d4ef842dc71aebd
IamMushroom <eratoster@gmail.com>
2024-06-09 13:26:59 +0400

    fix error when argument is d20 instead 1d20

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,6 +4,6 @@ def normalize_input(string: str) -> Tuple[int, int]:
     r = string.split('d')    
     if len(r) == 1:
         r = string.split('к')
-    if len(r) == 1:
-        return (0,0)
+    if len(r) == 1: return (0, 0)
+    if r[0] == '': return (1, int(r[1]))
     return (int(r[0]), int(r[1]))
