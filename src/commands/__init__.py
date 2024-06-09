51f48c63e15fc4c0a65dca532902c33023f42124
IamMushroom <eratoster@gmail.com>
2024-06-09 12:28:41 +0400

    Add logging

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -2,10 +2,14 @@ from dice.roll import roll_regular, roll_d20
 from utils import normalize_input
 from telegram import Update
 from telegram.ext import ContextTypes
+import logging
+import inspect
 
 async def roll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Roll cubes. Example: /roll 2d6, /roll 8к20
     """
+    frame = inspect.currentframe()
+    f_name = frame.f_code.co_name # type: ignore
     input: str = update.message.text # type: ignore
     try: input = input.split(' ')[1]
     except:
@@ -13,13 +17,16 @@ async def roll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             chat_id = update.effective_chat.id, # type: ignore
             text = 'Нет аргумента. Примеры: /roll 2d6, /roll 8к20',
             reply_to_message_id = update.effective_message.id) # type: ignore
+        logging.error(f'"chat_id": "{update.effective_chat.id}", "function": "{f_name}", "message": "leak of argument"') # type: ignore
         return
     ni = normalize_input(input)
     if ni[0] == 0:
         text = 'Не верный разделитель. Поддерживается либо латинская d (3d12), либо русская к (8к10)'
+        logging.warning(f'"chat_id": "{update.effective_chat.id}", "function": "{f_name}", "message": "wrong delimeter", "argument": "{input}"') # type: ignore
     else:
         r = roll_regular(ni[0], ni[1])
         text = f'Your roll: {sum(r)} ({" + ".join(map(str, r))})'
+        logging.info(f'"chat_id": "{update.effective_chat.id}", "function": "{f_name}", "argument": "{input}"') # type: ignore
     await context.bot.send_message(
         chat_id = update.effective_chat.id, # type: ignore
         text = text,
@@ -28,6 +35,8 @@ async def roll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def rolld20(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Roll cubes with increased chances for max and min roll. Example: /rolld20 2d6, /rolld20 8к20
     """
+    frame = inspect.currentframe()
+    f_name = frame.f_code.co_name # type: ignore
     input: str = update.message.text # type: ignore
     try: input = input.split(' ')[1]
     except:
@@ -35,13 +44,16 @@ async def rolld20(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             chat_id = update.effective_chat.id, # type: ignore
             text = 'Нет аргумента. Примеры: /roll 2d6, /roll 8к20',
             reply_to_message_id = update.effective_message.id) # type: ignore
+        logging.error(f'"chat_id": "{update.effective_chat.id}", "function": "{f_name}", "message": "leak of argument"') # type: ignore
         return
     ni = normalize_input(input)
     if ni[0] == 0:
         text = 'Не верный разделитель. Поддерживается либо латинская d (3d12), либо русская к (8к10)'
+        logging.warning(f'"chat_id": "{update.effective_chat.id}", "function": "{f_name}", "message": "wrong delimeter", "argument": "{input}"') # type: ignore
     else:
         r = roll_d20(ni[0], ni[1])
         text = f'Your roll: {sum(r)} ({" + ".join(map(str, r))})'
+        logging.info(f'"chat_id": "{update.effective_chat.id}", "function": "{f_name}", "argument": "{input}"') # type: ignore
     await context.bot.send_message(
         chat_id = update.effective_chat.id, # type: ignore
         text = text,
--- src/run.py
+++ src/run.py
@@ -2,14 +2,23 @@ from dotenv import load_dotenv
 from os import getenv
 from telegram.ext import ApplicationBuilder, CommandHandler, filters
 import commands
+import logging
+import log_format
 
 def main() -> None:
-    load_dotenv()
+    logging.info('"message": "Try to load token from TG_TOKEN env variable"')
     token: str = getenv('TG_TOKEN', '0')
+    logging.error('"message": "Can\'t load token"') if token == 0 else logging.info('"message": "Token has been successfuly loaded"')
     app = ApplicationBuilder().token(token).build()
     app.add_handler(CommandHandler("roll", commands.roll, filters.TEXT))
     app.add_handler(CommandHandler("rolld20", commands.rolld20, filters.TEXT))
+    logging.info('"message": "Application started"')
     app.run_polling()
 
 if __name__ == '__main__':
+    load_dotenv()
+    format: str = getenv('LOG_FORMAT', 'json')
+    logging.basicConfig(level=logging.INFO, format=log_format.format(format))
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+    logging.getLogger("telegram.ext.Application").setLevel(logging.WARNING)
     main()
