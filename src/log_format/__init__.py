4d6754d2ff216da940d83b57f064dfc1b54d7b1f
IamMushroom <eratoster@gmail.com>
2024-06-09 13:30:49 +0400

    fix json log format

--- src/log_format/__init__.py
+++ src/log_format/__init__.py
@@ -1,5 +1,5 @@
 def format(format: str) -> str:
     formats = {
-        "json": '"datetime": "%(asctime)s", "level": "%(levelname)s", "logger": "%(name)s", "message": {%(message)s}'
+        "json": '{"datetime": "%(asctime)s", "level": "%(levelname)s", "logger": "%(name)s", "message": {%(message)s}}'
     }
     return formats[format]
